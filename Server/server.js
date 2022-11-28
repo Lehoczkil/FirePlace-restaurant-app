8a076474b692108360bc2a367399504f4946bfd8
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-11-28 09:24:22 +0100

    feat: Create endpoint for getting meals from database

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
+const port = 3100;
 const app = express();
-const port = 3100
 
 // Enable commination with database
 const db = require("./queries");
+
+// Endpoint for getting all meals from db
+app.get("/meals", async (req, res) => {
+  let meals = await db.getMeals();
+  return res.send(meals);
+});
 
 app.listen(port);
