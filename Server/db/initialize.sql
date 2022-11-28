ad4eb1aa8109738d9d989ca711b6f39cc243f86e
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-11-28 09:23:05 +0100

    fix: Initialization of database

--- Server/db/initialize.sql
+++ Server/db/initialize.sql
@@ -1,77 +1,65 @@
-INSERT INTO customers (id, name, email)
+INSERT INTO customers ( name, email)
     VALUES(
-        1,
         'Joshua Mayers',
         'mayers@gmail.com'
-    )
+    );
     
-INSERT INTO customers (id, name, email)
+INSERT INTO customers ( name, email)
     VALUES(
-        2,
         'Alex Baldwin',
         'baldwin@gmail.com'
-    )
+    );
 
-
-INSERT INTO meals (id, name, price)
+INSERT INTO meals ( name, price)
     VALUES(
-        1,
         'arrabiata',
         6.99
-    )
-
-INSERT INTO meals (id, name, price)
+    );
+    
+INSERT INTO meals ( name, price)
     VALUES(
-        2,
         'falafel-tortilla',
         9.99
-    )
+    );
 
-INSERT INTO meals (id, name, price)
+INSERT INTO meals (name, price)
     VALUES(
-        3,
         'fried-fish',
         11.99
-    )
+    );
 
-INSERT INTO meals (id, name, price)
+INSERT INTO meals (name, price)
     VALUES(
-        4,
         'hamburger',
         11.99
-    )
+    );
 
-INSERT INTO meals (id, name, price)
+INSERT INTO meals (name, price)
     VALUES(
-        5,
         'medium-steak-with-fries',
         14.99
-    )
+    );
 
-INSERT INTO meals (id, name, price)
+INSERT INTO meals (name, price)
     VALUES(
-        6,
         'rare-steak-and-vegetables',
         15.99
-    )
+    );
 
-INSERT INTO meals (id, name, price)
+INSERT INTO meals (name, price)
     VALUES(
-        7,
         'shakshuka',
         9.99
-    )
+    );
 
-INSERT INTO meals (id, name, price)
+INSERT INTO meals (name, price)
     VALUES(
-        8,
         'shish-kebab-and-vegetables',
         10.99
-    )
+    );
 
-INSERT INTO meals (id, name, price)
+INSERT INTO meals (name, price)
     VALUES(
-        9,
         'vegan-salad-quinoa-bowl',
         16.99
-    )
+    );
