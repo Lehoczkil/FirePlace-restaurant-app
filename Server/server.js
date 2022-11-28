e0682607bc66bda8e87d291fc5ee7ebc2550d58f
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-11-28 09:29:54 +0100

    refactor: Rename endpoints in server

--- Server/server.js
+++ Server/server.js
@@ -6,19 +6,19 @@ const app = express();
 const db = require("./queries");
 
 // Endpoint for getting all meals from db
-app.get("/meals", async (req, res) => {
+app.get("/api/meals", async (req, res) => {
   let meals = await db.getMeals();
   return res.send(meals);
 });
 
 // Endpoint for getting all customers from db
-app.get("/customers", async (req, res) => {
+app.get("/api/customers", async (req, res) => {
   let customers = await db.getCustomers();
   return res.send(customers);
 });
 
 // Endpoint for getting all orders from db
-app.get("/orders", async (req, res) => {
+app.get("/api/orders", async (req, res) => {
     let orders = await db.getOrders();
     return res.send(orders);
   });
