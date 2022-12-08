df912136ed35c0d0b7bb8bb78258204838c74d87
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-12-08 16:15:04 +0100

    style: Refine header and make it responsive

--- vue-client/src/components/Header.vue
+++ vue-client/src/components/Header.vue
@@ -1,79 +1,141 @@
 <template>
     <div>
         <header>
-            <div class="pages">
+            <i class="fas fa-bars" @click="showMenu()" />
+            <div class="nav-content" :class="this.showMobileMenu ? 'open-menu' : 'closed-menu'">
                 <img src="../assets/logo.png" alt="logo">
-                <button class="btn-meals">Meals</button>
-            </div>
-            <div class="functionalities">
-                <button class="btn-logout">Logout</button>
-                <button class="btn-cart">Cart</button>
+                <div class="nav-items">
+                    <button class="menu">Menu</button>
+                    <div class="settings">
+                        <button>About</button>
+                        <button>Cart</button>
+                        <button>Logout</button>
+                    </div>
+                </div>
             </div>
         </header>
     </div>
 </template>
 
-<script>
-export default {
-    name: 'Header'
-}
-</script>
 
 <style scoped>
 header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-    min-height: 10vh;
-    width: 100vw;
-    background: black;
-}
-
-img {
-    width: 7vw;
+    background-color: black;
 }
 
 button {
-    width: clamp(100px, 9vw, 500px);
+    background: transparent;
     border: none;
     border-radius: clamp(5px, 0.5vh, 30px);
-    padding-block: 0.8vh;
-    font-size: clamp(15px, 1vw, 100px);
-    background: crimson;
     color: white;
-    ;
     cursor: pointer;
-    font-family: inherit;
+    font-size: clamp(15px, 1vw, 100px);
     font-weight: 600;
-    transition: all 0.3s;
+    padding-block: 0.8vh;
+    transition: all 0.4s;
+    width: clamp(100px, 9vw, 500px);
 }
 
 button:hover {
+
     background: white;
     color: black;
 }
 
 button:active {
-    transform: scale(0.98);
+    transform: scale(0.95);
 }
 
-.btn-meals {
+img {
+    width: clamp(100px, 7vw, 300px)
+}
+
+i {
+    display: none;
+}
+
+.menu {
     background: darkgoldenrod;
-    color: white;
     width: clamp(150px, 15vw, 700px);
 }
 
-.pages,
-.functionalities {
-    display: flex;
+.nav-content {
     align-items: center;
+    display: flex;
     justify-content: space-between;
-    width: 20vw;
-    margin-inline: clamp(20px, 10vw, 100px);
+    padding: 10px 30px;
 }
 
-.pages {
-    width: 27vw;
+.nav-items {
+    align-items: center;
+    display: flex;
+    flex-basis: 80%;
+    justify-content: space-between;
+    margin: 0;
+    padding: 0;
+}
+
+.settings {
+    display: flex;
+    justify-content: space-between;
+    width: clamp(350px, 36vw, 1000px);
+}
+
+@media screen and (max-width: 768px) {
+    header {
+        width: 100%;
+    }
+
+    .open-menu {
+        min-height: 150px;
+        opacity: 1;
+    }
+
+    .closed-menu {
+        height: 0;
+        opacity: 0;
+        padding: 0;
+    }
+
+    .nav-content,
+    .nav-items {
+        flex-direction: column;
+    }
+
+    i {
+        color: white;
+        display: block;
+        font-size: clamp(30px, 2vw, 100px);
+        padding: 20px;
+        text-align: right;
+    }
+
+    .settings {
+        flex-direction: column;
+        width: 100%;
+    }
+
+    button,
+    .menu {
+        margin: auto;
+        width: clamp(200px, 50vw, 400px);
+    }
 }
 </style>
+
+
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+            showMobileMenu: false,
+        };
+    },
+    methods: {
+        showMenu() {
+            this.showMobileMenu = !this.showMobileMenu;
+        },
+    },
+}
+</script>
