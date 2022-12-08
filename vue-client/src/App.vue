29d2c7b19de9f19258594cc9bb658528116cbeb3
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-12-08 09:49:38 +0100

    style: Add basic styling to header

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
-    <Header/>
+    <Header />
     <router-view></router-view>
-    <Footer/>
+    <Footer />
   </div>
 </template>
 
@@ -19,5 +19,19 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
 
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Poppins', sans-serif;
+}
+
+.container {
+  min-height: 100vh;
+}
 </style>
--- vue-client/src/components/Header.vue
+++ vue-client/src/components/Header.vue
@@ -1,7 +1,16 @@
 <template>
-    <header>
-        <h1>FirePlace</h1>
-    </header>
+    <div>
+        <header>
+            <div class="pages">
+                <img src="../assets/logo.png" alt="logo">
+                <button class="btn-meals">Meals</button>
+            </div>
+            <div class="functionalities">
+                <button class="btn-logout">Logout</button>
+                <button class="btn-cart">Cart</button>
+            </div>
+        </header>
+    </div>
 </template>
 
 <script>
@@ -11,5 +20,60 @@ export default {
 </script>
 
 <style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    min-height: 10vh;
+    width: 100vw;
+    background: black;
+}
 
+img {
+    width: 7vw;
+}
+
+button {
+    width: clamp(100px, 9vw, 500px);
+    border: none;
+    border-radius: clamp(5px, 0.5vh, 30px);
+    padding-block: 0.8vh;
+    font-size: clamp(15px, 1vw, 100px);
+    background: crimson;
+    color: white;
+    ;
+    cursor: pointer;
+    font-family: inherit;
+    font-weight: 600;
+    transition: all 0.3s;
+}
+
+button:hover {
+    background: white;
+    color: black;
+}
+
+button:active {
+    transform: scale(0.98);
+}
+
+.btn-meals {
+    background: darkgoldenrod;
+    color: white;
+    width: clamp(150px, 15vw, 700px);
+}
+
+.pages,
+.functionalities {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 20vw;
+    margin-inline: clamp(20px, 10vw, 100px);
+}
+
+.pages {
+    width: 27vw;
+}
 </style>
